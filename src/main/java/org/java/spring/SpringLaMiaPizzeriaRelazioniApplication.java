54b87c264565b3de28468c54d98023d17f682a75
alep-2000 <alessiopiras056@gmail.com>
2023-12-07 12:51:48 +0100

    first commit

---
+++ src/main/java/org/java/spring/SpringLaMiaPizzeriaRelazioniApplication.java
@@ -0,0 +1,35 @@
+package org.java.spring;
+
+import org.java.spring.db.pojo.Pizza;
+import org.java.spring.db.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaRelazioniApplication implements CommandLineRunner{
+
+	@Autowired
+	private PizzaService pizzaService;
+		
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaRelazioniApplication.class, args);
+		
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		
+		pizzaService.save(new Pizza("Margherita", "Una buona pizza con ingredienti freschi", "https://picsum.photos/200", 6.99));
+		pizzaService.save(new Pizza("Marinara", "Una buona pizza con ingredienti freschi", "https://picsum.photos/200/300", 8));
+		pizzaService.save(new Pizza("Bufala", "Una buona pizza con ingredienti freschi", "https://picsum.photos/200/300", 18));
+		pizzaService.save(new Pizza("Funghi", "Una buona pizza con ingredienti freschi", "https://picsum.photos/200/300", 15.50));
+		pizzaService.save(new Pizza("Salsiccia", "Una buona pizza con ingredienti freschi", "https://picsum.photos/200/300", 6.30));
+		pizzaService.save(new Pizza("Capricciosa", "Una buona pizza con ingredienti freschi", "https://picsum.photos/200/300", 10.50));
+		pizzaService.save(new Pizza("Carbonara", "Una buona pizza con ingredienti freschi", "https://picsum.photos/200/300", 22.80));
+		pizzaService.save(new Pizza("4 Formaggi", "Una buona pizza con ingredienti freschi", "https://picsum.photos/200/300", 10.50));
+	}
+}
---
+++ src/main/java/org/java/spring/db/pojo/Pizza.java
@@ -0,0 +1,82 @@
+package org.java.spring.db.pojo;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+
+@Entity
+public class Pizza {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(length = 60)
+	@Length(min = 3, max = 60, message = "Il nome deve essere compreso tra 3 e 60 caratteri")
+	@NotBlank(message = "Il nome non può essere nullo")
+	private String nome;
+	
+	@Column(columnDefinition = "TEXT")
+	@Length(min = 3, message = "La descrizione deve essere lungo minino 3 caratteri")
+	@NotBlank(message = "La descrizione non può essere nulla")
+	private String descrizione;
+	
+	@URL(protocol = "https")
+	@NotBlank(message = "Url non può essere nullo")
+	private String foto;
+	
+	@PositiveOrZero(message = "Puoi inserire solamente un numero positivo o uguale a 0") 
+	private double prezzo;
+	
+	public Pizza() { }
+	public Pizza(String nome, String descrizione, String foto, double prezzo) {
+		
+		setNome(nome);
+		setDescrizione(descrizione);
+		setFoto(foto);
+		setPrezzo(prezzo);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	@Override
+	public String toString() {
+		
+		return "[" + getId() + "] " + getNome() + " - " 
+				+ getFoto() + " (" + getPrezzo() + ")";
+	}
+}
